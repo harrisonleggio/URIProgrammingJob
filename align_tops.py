3539e80d97bc3e164b9f61dc66801b67835e7fa7
Harrison Leggio <harrisonleggio@my.uri.edu>
2017-03-21 11:16:30 -0400

    Update align_tops.py

--- align_tops.py
+++ align_tops.py
@@ -3,6 +3,7 @@ import sys
 from shutil import copyfile
 from subprocess import Popen, PIPE
 import subprocess
+from make_dem import file_finder
 
 directory = "/Users/student/Desktop/Task-3-7-2017"
 os.chdir(directory)
@@ -19,9 +20,20 @@ master_image = content[3].split('= ')[1]
 slave_image = content[4].split('= ')[1]
 master_orbit = content[5].split('= ')[1]
 slave_orbit = content[6].split('= ')[1]
-topo_range = content[6].split('= ')[1]
-swath_number = content[7].split('= ')[1]
+topo_range = content[7].split('= ')[1]
+swath_number = content[8].split('= ')[1]
 
-os.mkdir(work_folder + '/topo')
-os.mkdir(work_folder + '/orig')
+topo_path = work_folder + '/topo'
+orig_path = work_folder + '/orig'
+
+os.mkdir(topo_path)
+os.mkdir(orig_path)
+
+long1, long2, lat1, lat2 = topo_range.split('/')
+
+os.chdir('/shareddata/GRD_SRTM4.1')
+
+file_finder(float(long1), float(long2), float(lat1), float(lat2), topo_path)
+
+
 
